503ebdad7a05253f27d30402176637f80d8b56d8
Wooram Jun <chatii2412@gmail.com>
2017-09-09 14:29:24 +0900

    Chapter 5: array, tuple

--- output/type.js
+++ output/type.js
@@ -8,3 +8,9 @@ isReady = isOk; // Failed.
 /* undefined & null are subtypes of all other types. */
 var isReady2 = null;
 var isReady3 = null; // strictNullChecks: true
+/* array, tuple */
+var array1 = [1];
+var array2 = [1];
+var array3 = [1];
+var tuple = ["1", 2, "3", 4];
+var t0 = tuple[0]; // type inferencable
--- src/type.ts
+++ src/type.ts
@@ -12,3 +12,11 @@ isReady = isOk; // Failed.
 let isReady2: boolean = null;
 let isReady3: boolean | null = null; // strictNullChecks: true
 
+/* array, tuple */
+let array1: Array<number> = [1];
+let array2: number[] = [1];
+let array3: Array<number | string> = [1];
+
+let tuple: [string, number] = ["1", 2, "3", 4];
+
+let t0 = tuple[0]; // type inferencable
